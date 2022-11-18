d9812f95f426bc2c8972a8a1cb2e809f11626005
Rizky Sanjaya T <sanjayatandia@gmail.com~>
2022-11-19 00:43:19 +0700

    Adjusment

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -44,7 +44,7 @@ class Autoload extends AutoloadConfig
         APP_NAMESPACE => APPPATH, // For custom app namespace
 		'Config'      => APPPATH . 'Config',
 		'App'         => APPPATH,
-		'Myth\Auth'   => APPPATH .'ThirdParty/myth-auth/src',
+		
     ];
 
     /**
